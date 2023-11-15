3e9c3234c6fcac186baa144da3fec0ae5f957a94
Medjai229 <115402766+Medjai229@users.noreply.github.com>
2023-11-15 11:44:06 +0200

    Update main.h

--- main.h
+++ main.h
@@ -65,54 +65,35 @@ typedef struct specifier
 	int (*f)(va_list, params_t *);
 } specifier_t;
 
-/* _put.c module */
 int _puts(char *str);
 int _putchar(int c);
-
-/* print_functions.c module */
 int printc(va_list ap, params_t *params);
 int printi(va_list ap, params_t *params);
 int prints(va_list ap, params_t *params);
 int printp(va_list ap, params_t *params);
 int printS(va_list ap, params_t *params);
-
-/* number.c module */
 char *conv(long int num, int base, int flags, params_t *params);
 int printuns(va_list ap, params_t *params);
 int printaddr(va_list ap, params_t *params);
-
-/* specifier.c module */
 int (*getspec(char *s))(va_list ap, params_t *params);
 int getprint_func(char *s, va_list ap, params_t *params);
 int getf(char *s, params_t *params);
 int getmod(char *s, params_t *params);
 char *getwidth(char *s, params_t *params, va_list ap);
-
-/* conv_number.c module */
 int printhex(va_list ap, params_t *params);
 int printHEX(va_list ap, params_t *params);
 int printbin(va_list ap, params_t *params);
 int printoct(va_list ap, params_t *params);
-
-/* simple_printers.c module */
 int print_from_to(char *start, char *stop, char *except);
 int printrev(va_list ap, params_t *params);
 int printrot13(va_list ap, params_t *params);
-
-/* printnum.c module */
 int _isdigit(int c);
 int _strlen(char *s);
 int printnum(char *str, params_t *params);
 int printnum_right_shift(char *str, params_t *params);
 int printnum_left_shift(char *str, params_t *params);
-
-/* params.c module */
 void in_parm(params_t *params, va_list ap);
-
-/* string_fields.c modoule */
 char *getprec(char *p, params_t *params, va_list ap);
-
-/* _prinf.c module */
 int _printf(const char *format, ...);
 
 #endif /*MAIN_H*/
